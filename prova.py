42947316b45d4a6ce3a35bc3119072a4101a3a9f
lucab14 <115220993+lucab14@users.noreply.github.com>
2023-01-27 18:59:24 +0000

    Correzione codice

--- prova.py
+++ prova.py
@@ -1,11 +1,31 @@
 def sum_csv(file):
   values=[]
   for line in file:
-    elements= line.split(',')
-    if elements[0]!='Data':
-      value=elements[1]
+    elements=line.split(',')
+    if elements[0] != 'Date' :
+      value=float(elements[1])
       values.append(value)
 
   file.close()
   return sum(values)
+
+
+file=open('shampoo_sales.csv', 'r')
+print(sum_csv(file))
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+  
+#file="shampoo_sales.csv"
   

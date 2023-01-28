ce0e73a1c81391e57945690d625017fdb174cc01
lucab14 <115220993+lucab14@users.noreply.github.com>
2023-01-28 08:43:09 +0000

    Correzione

--- prova.py
+++ prova.py
@@ -1,12 +1,32 @@
-def sum_csv(file_name):
+def sum_list(lista):
+  if not len(lista):
+    return None
+  return sum(lista)
+
+
+def read_csv(file_name):
   my_file=open(file_name, 'r')
   values=[]
   for line in my_file:
-    elements=line.split(',')
-    if elements[0] != 'Date' :
-      value=float(elements[1])
-      values.append(value)
+    elements= line.split(',')
+    if elements[0]!='Date':
+      values.append(elements[1])
+  return values
 
-  my_file.close()
-  return sum(values)
+def convert_list(values):
+  list=[]
+  for item in values:
+    try:
+      list.append(float(item))
+    except:
+      None
+  return list
 
+def sum_csv(file_name):
+  file=read_csv(file_name)
+  list=convert_list(file)
+  sum_file=sum_list(list)
+  return sum_file
+
+nome='shampoo_sales.csv'
+print(sum_csv(nome))
